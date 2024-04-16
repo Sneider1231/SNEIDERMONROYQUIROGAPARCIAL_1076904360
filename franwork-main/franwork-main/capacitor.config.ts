b6d6a316af8fc3d78c0a3fd5a3cb5829303b4581
Sneider1231 <127459662+Sneider1231@users.noreply.github.com>
2024-04-16 07:04:22 -0500

    SNEIDERMONROYQUIROGAPARCIAL_1076904360

---
+++ franwork-main/franwork-main/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'franwork',
+  webDir: 'dist'
+};
+
+export default config;
